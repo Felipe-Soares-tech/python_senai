a4e8e3cb71b5adc7bed7e659e403d93ff8c3f87f
felipe soares <felipesoaressiqueira@gmail.com>
2024-09-02 17:49:41 -0300

    pqpppppppppppp

---
+++ teste.py
@@ -0,0 +1,12 @@
+from time import time
+
+
+numeros = [[1,2,3,4],[5,4534,3],[65,34,5]]
+
+
+for numero in numeros:
+    print(numero)
+
+print(numeros[0][3])
+
+numeros = [[1,2,3],[5,4534,3],[65,34,5]]
